d1c5b628ec7492742a17ab08abb7ba88213d5d39
“Alison <alison.obright@gmail.com>
2023-11-05 19:30:47 -0600

    uploading new project version

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Canvas",p:["p1"]},{w:"overview",p:["p1","p2"]},{w:"Kaltura",p:["p2"]},{w:"Recording",p:["p3"]},{w:"new",p:["p3"]},{w:"media",p:["p3","p5"]},{w:"Video",p:["p4"]},{w:"share",p:["p4"]},{w:"settings",p:["p4"]},{w:"table",p:["p4"]},{w:"Uploading",p:["p5"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4];
+ph["p2"]=[5, 4];
+ph["p3"]=[6, 7, 8];
+ph["p4"]=[9, 10, 11, 12];
+ph["p5"]=[13, 8];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {3:2,5:2,4:5,2:-1,1:-1};
